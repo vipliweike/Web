1aff8910f8c9ceffe7d123e1ed9ea978b225ada3
vipliweike <18731199378qwe>
2018-11-22 11:57:34 +0800

    添加项目文件。

---
+++ DataProject/MySqlCommands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DataProject
+{
+    public class MySqlCommands
+    {
+        public static DataTable GetData(string name)
+        {
+            string sql = @"SELECT * FROM mytesttable where Test_Name=?Name";
+            MySqlParameter[] parms= {
+                new MySqlParameter("?Name", MySqlDbType.VarChar, 200),
+            };
+            parms[0].Value = name;
+            DataTable table = DbHelperForMySQL.Query(sql,parms).Tables[0];
+            return table;
+        }
+
+        public static DataTable GetData()
+        {
+            string sql = @"select * from mytesttable";
+            DataTable table = DbHelperForMySQL.Query(sql).Tables[0];
+            return table;
+        }
+    }
+}
---
+++ TestWebAPI/Controllers/MySQLTestController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace TestWebAPI.Controllers
+{
+    public class MySQLTestController : ApiController
+    {
+        [Route("api/GetTestMySQL")]
+        [HttpGet]
+        public HttpResponseMessage GetTestMySQL(string Test_Name)
+        {
+
+            StringBuilder str = new StringBuilder();
+            str.Append("[");
+            DataTable temp = DataProject.MySqlCommands.GetData(Test_Name);
+            foreach (DataRow row in temp.Rows)
+            {
+                str.Append("{\"Test_Id\":");
+                str.Append("\"" + row["Test_Id"].ToString() + "\",");
+                str.Append("\"Test_Name\":" + "\"" + row["Test_Name"].ToString() + "\",");
+                str.Append("\"create_time\":" + "\"" + row["create_time"].ToString() + "\",");
+                str.Append("\"remark\":" + "\"" + row["remark"].ToString() + "\"");
+                str.Append("},");
+            }
+            str.Append("]");
+            HttpResponseMessage result = new HttpResponseMessage();
+            result.Content = new StringContent(str.ToString(), Encoding.GetEncoding("UTF-8"), "application/json");
+            return result;
+            //return Json(new { data=temp});
+        }
+
+        [Route("api/GetTestMySQLNoCan")]
+        [HttpGet]
+        public HttpResponseMessage GetTestMySQLNoCan()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append("[");
+            DataTable temp = DataProject.MySqlCommands.GetData();
+            foreach (DataRow row in temp.Rows)
+            {
+                str.Append("{\"Test_Id\":");
+                str.Append("\"" + row["Test_Id"].ToString() + "\",");
+                str.Append("\"Test_Name\":" + "\"" + row["Test_Name"].ToString() + "\",");
+                str.Append("\"create_time\":" + "\"" + row["create_time"].ToString() + "\",");
+                str.Append("\"remark\":" + "\"" + row["remark"].ToString() + "\"");
+                str.Append("},");
+            }
+            str.Append("]");
+            HttpResponseMessage result = new HttpResponseMessage();
+            result.Content = new StringContent(str.ToString(), Encoding.GetEncoding("UTF-8"), "application/json");
+            return result;
+        }
+    }
+}
